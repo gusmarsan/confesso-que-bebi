b2f12340f5054ed70d005f01ecc1ed6df389e41b
gusmarsan <gustavomarsan@gmail.com>
2026-08-09 20:46:28 -0300

    Bump Android version to v0.5.2

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.gusmarsan.confessoquebebi"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.5"
+        versionCode = 6
+        versionName = "0.5.2"
     }
 
     buildTypes {
